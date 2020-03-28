e36cd5336e2d817990a72bc0f03073e12d3d6dd8
Shaun Gedye <sgedye@Shauns-MBP.modem>
2020-03-28 15:51:24 +0800

    Updates

---
+++ precache-manifest.a482ed7a58c8298f9c9b13decc8a5a01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6eba616f0d152cad893b8186d1b9153",
+    "url": "/github.io/cv/index.html"
+  },
+  {
+    "revision": "2e87bfb3891685fe369b",
+    "url": "/github.io/cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8ca3aec3a0e1491d614",
+    "url": "/github.io/cv/static/js/2.a144b7c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/cv/static/js/2.a144b7c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e87bfb3891685fe369b",
+    "url": "/github.io/cv/static/js/main.7f7ed004.chunk.js"
+  },
+  {
+    "revision": "1549876056fd25dbdb8e",
+    "url": "/github.io/cv/static/js/runtime-main.40548ab3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
